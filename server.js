c4bea4ab4517edb0459b4522f0b33254b304cd8f
Puranjoy Saha <p9saha@outlook.com>
2020-10-03 14:45:34 -0600

    recognize other user connecting

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const { response } = require('express');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-
+const io = require('socket.io')(server)
 const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+})
+
+
+
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -15,6 +23,12 @@ app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room });
 })
 
+io.on('connection', socket => {
+    socket.on('join-room', (roomId) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected');
+    })
+})
 
 
 server.listen(3030);
